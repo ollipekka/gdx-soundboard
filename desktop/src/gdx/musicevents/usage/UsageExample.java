233eccf5b41dfe1922b9b43e35341ce9a67e3c76
ollipekka <ollipekka@gmail.com>
2015-02-06 19:02:16 +0200

    Added usage example.

---
+++ desktop/src/gdx/musicevents/usage/UsageExample.java
@@ -0,0 +1,94 @@
+package gdx.musicevents.usage;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.gdx.musicevents.MusicEventManager;
+
+public class UsageExample extends ApplicationAdapter {
+    
+    private final MusicEventManager eventManager = new MusicEventManager();
+    private Skin skin;
+    private Stage stage;
+
+    public enum GameEvent{
+        RISING,
+        CALM
+    }
+    @Override
+    public void create() {
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        stage = new Stage(new ScreenViewport());
+        
+        final Dialog dialog = new Dialog("Events",skin);
+        
+        Table contentTable = dialog.getContentTable();
+
+        TextButton rising = new TextButton("RISING", skin);
+        contentTable.add(rising).fill().expand().row();
+        rising.addListener(new ChangeListener() {
+            
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                eventManager.play(GameEvent.RISING);
+            }
+        });
+        
+        TextButton calm = new TextButton("CALM", skin);
+        contentTable.add(calm).fill().expand().row();
+        calm.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                eventManager.play(GameEvent.CALM);
+            }
+        });
+        
+        TextButton action = new TextButton("COMBAT", skin);
+        action.addListener(new ChangeListener() {
+            
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                eventManager.play("COMBAT");
+            }
+        });
+        
+        contentTable.add(action).fill().expand().row();
+        
+        Gdx.input.setInputProcessor(stage);
+        
+        dialog.show(stage);
+        
+        eventManager.load(Gdx.files.internal("music/music-events.json"));
+    }
+    
+    
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+
+        eventManager.update(Gdx.graphics.getRawDeltaTime());
+
+        Gdx.gl.glClearColor(0.35f, 0.35f, 0.45f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+
+    }
+
+}
